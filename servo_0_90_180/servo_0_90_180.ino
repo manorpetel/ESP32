134becf3140347c56d500f3401cd0ae73b8d0159
manorpetel <manorpetel@gmail.com>
2026-01-25 19:15:01 +0000

    Update servo_0_90_180.ino

--- servo_0_90_180/servo_0_90_180.ino
+++ servo_0_90_180/servo_0_90_180.ino
@@ -1,34 +1,23 @@
 #include <ESP32Servo.h>
 
-double angle_num;
+int angle;
 Servo servo_17;
 
-void setup()
-{
-  	pinMode(2, OUTPUT);
+void setup() {
 	servo_17.attach(4);
-
-	Serial.begin(115200);
-	Serial.flush();
-	//while(Serial.available()>0)Serial.read();
-
-	delay(1000);
-	digitalWrite(2, LOW);
 	servo_17.write(0);
-
+  Serial.begin(115200);
+	Serial.println("Enter servo angle (0-180)");
 }
 
-
-void loop()
-{
-	yield();
-
-  //Serial.println(String("Enter Angle (0-180)"));
-  angle_num = atof((Serial.readStringUntil('\n')).c_str());
-  servo_17.write(180);
-  delay(1000);
-  servo_17.write(90);
-  delay(1000);
-  servo_17.write(0);
-  delay(1000);
+void loop() {
+  
+  if (Serial.available() > 0) {
+    angle = Serial.parseInt();
+    Serial.print("The angle you entered is : ");
+    Serial.println(angle);
+		servo_17.write(angle);
+    delay(2000);
+		Serial.println("Enter servo angle (0-180)");
+  }
 }
